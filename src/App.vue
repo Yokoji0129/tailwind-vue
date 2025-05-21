8e475deee04ae234ec2d9a48a285827daecae1c7
Yokoji0129 <132248053+Yokoji0129@users.noreply.github.com>
2025-05-22 01:56:06 +0900

    環境構築

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+    <!-- テスト用のtailwindsを追加 -->
+    <div class="bg-purple-400 text-white text-xl">
+      これはTailwindv4でつくられました!
+    </div> 
+</template>
+
+<style scoped>
+
+</style>
